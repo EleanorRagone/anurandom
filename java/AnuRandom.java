faee37bf509152833dc08e1ea422f9294f9e3f25
Kristian Lundkvist <kristian.lundkvist@gmail.com>
2012-04-24 14:08:47 +0200

    Added documentation.

--- java/AnuRandom.java
+++ java/AnuRandom.java
@@ -1,15 +1,38 @@
 import java.io.*;
 import java.net.*;
 
+/**
+@author Kristian Lundkvist <kristian.lundkvist@gmail.com>
+*/
+
+/**
+A class for downloading and parsing a 1024 bytes of random data from http://150.203.48.55/RawChar.php
+*/
 public class AnuRandom{
 
+	/**
+	DataInputStream using for reading from the website
+	*/
 	private DataInputStream in;
 
+	/**
+	Variable temporary storing the webpage
+	*/
 	private String page;
+
+	/**
+	Stores the bytes as a String variable
+	*/
 	private String bytes;
 
+	/**
+	The number of bytes to download
+	*/
 	private int numberOfBytes;
 
+	/**
+	Standard constructor
+	*/
 	public AnuRandom(){
 		this.in = null;
 		this.page = "";
@@ -17,6 +40,10 @@ public class AnuRandom{
 		this.numberOfBytes = 0;
 	}
 
+	/**
+	Constructor
+	@param The number of bytes to download
+	*/
 	public AnuRandom(int numberOfBytes){
 		this.numberOfBytes = numberOfBytes;
 		this.in = null;
@@ -24,22 +51,33 @@ public class AnuRandom{
 		this.bytes = "";
 	}
 
+	/**
+	Set the number of bytes
+	@param The new number of bytes
+	*/
 	public void setNumberOfBytes(int numberOfBytes){
 		this.numberOfBytes = numberOfBytes;
 	}
 
+	/**
+	Returns the number of bytes
+	@return The number of bytes
+	*/
 	public int getNumberOfBytes(){
 		return this.numberOfBytes;
 	}
 
-	public String getPageString(){
-		return this.page;
-	}
-
+	/**
+	Returns the bytes from the webpage
+	@return The downloaded bytes
+	*/
 	public byte[] getBytes(){
 		return this.bytes.getBytes();
 	}
 
+	/**
+	Downloads the webpage and stores it in memory
+	*/
 	public void getPage(){
 		try{
 			URL u = new URL("http://150.203.48.55/RawChar.php");
@@ -67,6 +105,9 @@ public class AnuRandom{
 		}
 	}
 
+	/**
+	Parses the random bytes from the webpage.
+	*/
 	public void parsePage(){
 
 		int start;
