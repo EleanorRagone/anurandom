f0a44346ea8231f585649785b5ce959b9770127b
Caitlin Maclatte <macleod.caitlin@gmail.com>
2013-09-04 21:16:46 +1000

    Revision after advice
    Got advice from RabidTech's Rob and Mermop.

--- anurandom.rb
+++ anurandom.rb
@@ -2,38 +2,30 @@ require 'open-uri'
 
 class ANURandom
 
-  BINARY = "BINARY"
-  HEX = "HEX"
-  CHAR = "CHAR"
+  def getRandom(type="BINARY")
 
-  def getRandom(self, type=BINARY)
-
-    if type == self.BINARY
+    if type == "BINARY"
       url = "http://150.203.48.55/RawBin.php"
-    elsif type == self.HEX
+    elsif type == "HEX"
       url = 'http://150.203.48.55/RawHex.php'
-    elsif type == self.CHAR
+    elsif type == "CHAR"
       url = 'http://150.203.48.55/RawChar.php'
     end
-
     page_contents = open(url,) { |f| f.read }
-
-    num = page_contents.split('"rng"')[1].split('<td>\n')[1].split('</td>')[0]
-
+    num = page_contents.split('"rng"')[1].split("<td>\n")[1].split('</td>')[0]
     return num
-
   end
 
-  def getBin(self)
-    getRandom self, BINARY
+  def getBin()
+    getRandom "BINARY"
   end
 
-  def getHex(self)
-    getRandom self, HEX
+  def getHex()
+    getRandom "HEX"
   end
 
-  def getChar(self)
-    getRandom self, CHAR
+  def getChar()
+    getRandom "CHAR"
   end
 
 end
