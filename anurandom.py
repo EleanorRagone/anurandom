7a60244d6a5c2f42ed6a17ccfac486d194efe13d
Benjamin George Roberts <benjamin@bgroberts.id.au>
2018-12-20 19:49:18 +1100

    updated anurandom.py to run on both py2 and py3

--- anurandom.py
+++ anurandom.py
@@ -1,4 +1,9 @@
-import urllib2
+import sys
+
+if sys.version_info >= (3,0):
+    from urllib.request import urlopen
+else:
+    from urllib2 import urlopen
 
 class ANURandom:
     BINARY = "BINARY"
@@ -13,10 +18,10 @@ class ANURandom:
         elif type == self.CHAR:
             url = 'http://150.203.48.55/RawChar.php'
 
-        page = urllib2.urlopen(url, timeout=5)
+        page = urlopen(url, timeout=5)
 
         data = page.read()
-        num = data.split('"rng"')[1].split('<td>\n')[1].split('</td>')[0]
+        num = data.decode().split('"rng"')[1].split('<td>\n')[1].split('</td>')[0]
         return num
 
     def getBin(self):
